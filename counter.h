696d4c8df770f1be3024cc67a23ce4e855f2d2fb
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-11-03 14:33:01 -0800

    Add tt code

---
+++ counter.h
@@ -0,0 +1,14 @@
+//
+//  counter.h
+//  reqroute
+//
+//  Created by Andrew Thomson on 10/28/13.
+//  Copyright (c) 2013 Andrew Thomson. All rights reserved.
+//
+
+#ifndef __reqroute__counter__
+#define __reqroute__counter__
+
+#include <iostream>
+
+#endif /* defined(__reqroute__counter__) */
